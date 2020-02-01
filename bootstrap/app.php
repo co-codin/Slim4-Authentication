c8a009df52dee3b3c375dff9c8d91b0291587cde
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-02 00:50:41 +0300

    sentinel authentication built

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,14 @@ use League\Container\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+\Cartalyst\Sentinel\Native\Facades\Sentinel::instance(
+    new \Cartalyst\Sentinel\Native\SentinelBootstrapper(
+        require(__DIR__ . '/../config/auth.php')
+    )
+);
+
+require_once __DIR__ . '/database.php';
+
 $container = new Container();
 
 AppFactory::setContainer($container);
@@ -12,6 +20,5 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 require_once __DIR__ . '/container.php';
-require_once __DIR__ . '/database.php';
 
 require_once __DIR__ . '/../routes/web.php';
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 $app->get('/', function ($request, $response, $args) use ($container) {
-    return $container->get('view')->render($response, 'pages/home/index.twig');
+    return $response;
 });
