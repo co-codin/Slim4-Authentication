888128dda1a0789192b0d0a3d660a1e0405c7562
ElijahCoding <cuiyeqing960904@gmail.com>
2020-02-03 21:30:54 +0300

    building signup

---
+++ app/Controllers/Auth/SignUpController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\Controller;
+
+class SignUpController extends Controller
+{
+    protected $view;
+
+    protected $flash;
+
+    protected $routeParser;
+
+    public function __construct()
+    {
+    }
+
+}
